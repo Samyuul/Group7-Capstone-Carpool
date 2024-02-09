2904c7ac67cf075c614e1d8c678d4eab803579f0
VincentGuoW <sdevincentg@gmail.com>
2024-02-09 01:40:43 -0500

    different file

--- backend/pages/login.js
+++ backend/pages/login.js
@@ -1,24 +1,5 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-var myWebsite = express();
+import "../server"
 
-const upload = require("express-fileupload");
-myWebsite.use(upload());
-
-myWebsite.use(express.urlencoded({extended:true}));
-
-myWebsite.set("views",path.join(__dirname,"views")); //This section should change to cd../frontend/src/components
-myWebsite.use(express.static(__dirname+"/public"));//For css file but I think we don't need it
-myWebsite.set("view engine","ejs")
-
-const {check,validationResult}= require("express-validator");
-const { stringify } = require("querystring");
-
-mongoose.connect("mongodb://localhost:27017/vroom-room"),{
-    UserNewUrlParser: true,
-    UserUnifiedTopology:true
-}
 
 const Accounts = mongoose.model("Accounts",{
     username:String,
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
@@ -19,41 +21,3 @@ mongoose.connect("mongodb://localhost:27017/vroom-room"),{
     UserNewUrlParser: true,
     UserUnifiedTopology:true
 }
-
-const Accounts = mongoose.model("Accounts",{
-    username:String,
-    password:String
-})
-
-var session = require("express-session");
-myWebsite.use(session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true
-}));
-
-
-myWebsite.get("/login", function (req, res) {
-    res.render("login")
-})
-
-myWebsite.post("/login", function (req, res) {
-    var username = req.body.txtUsername;
-    var password = req.body.txtPassword;
-
-    console.log(`username:${username} & password:${password}`)
-
-    Accounts.findOne({ username: username, password: password }).then((Accounts) => {
-        console.log(`Accounts:${Accounts}`);
-        if (Accounts) {
-            req.session.username = Accounts.username;
-            req.session.userLoggedIn = true;
-            res.redirect("/profile")
-        }
-        else {
-
-        }
-    }).catch((err) => {
-        console.log(`Error: ${err}`);
-    })
-})
