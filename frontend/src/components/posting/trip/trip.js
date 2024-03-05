4d8731d62140322b4387e4920e0ac6635eabecc7
Sam Yang <hongjyang9403@gmail.com>
2024-03-05 14:32:18 -0500

    Added test case for trip post method

--- frontend/src/components/posting/trip/trip.js
+++ frontend/src/components/posting/trip/trip.js
@@ -259,7 +259,7 @@ const Trip = (props) => {
 
         // Convert date object into string for database
         var datesAsString = dates.map((date, i) => {
-            return new Date(date.month.name + " " + date.day + ", " + date.year)
+            return date.month.name + " " + date.day + ", " + date.year
         });
         
         // Convert waypoint object into string for database
@@ -287,11 +287,29 @@ const Trip = (props) => {
             desc: tripDesc,
             distance: distance,
             eta: duration,
-            accountID: "Sarah-Smith",
+            name: "Sarah Smith",
             tripID: uuidv4()
         }
 
-        console.log(newTrip);
+        var test = {
+            start:"Toronto, ON, Canada",
+            end:"Burlington, ON, Canada",
+            waypoints:["Guelph, ON, Canada"],
+            date:["March 14, 2024","March 20, 2024","March 21, 2024"],
+            depart:"14:24",
+            return:"17:25",
+            model:"Ford Focus",
+            type:"Hatch Back",
+            color:"Red",
+            plate:"ABC 1234",
+            luggage:[false,false,true,false],
+            seat:[false,false,true,false,false,false,false],
+            pref:[true,true,true,false,false],
+            desc:"This is a test description for entering into the system. Please ignore what this message says.",
+            distance:149840,
+            eta:7186,
+            name:"Sarah Smith",
+            tripID:"469ed35b-c8f1-42df-9edb-d83764564acd"}
 
     }
 
