860ca7260245062c679ff55f6ea9ca4729c61f92
Sam Yang <hongjyang9403@gmail.com>
2024-03-24 22:28:21 -0400

    Updated config

--- frontend/src/routes/http-common.js
+++ frontend/src/routes/http-common.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
-console.log(process.env.REACT_APP_URL_PROXY);
-
 export default axios.create({
-    baseURL: process.env.REACT_APP_URL_PROXY,
     headers: {
         "Content-Type": "application/json"
     }
